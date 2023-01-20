541d42e28e19d4518362236cf35ef34ba82d13d0
Nicola Paro <nicola.paro@gmail.com>
2023-01-20 19:35:29 +0100

    fix

--- NicolaParo.NetConf2022.NotificationSender.Api/InitializationPipelineExtensions.cs
+++ NicolaParo.NetConf2022.NotificationSender.Api/InitializationPipelineExtensions.cs
@@ -108,7 +108,7 @@ namespace NicolaParo.NetConf2022.NotificationSender.Api
 
                 return Results.Ok(result);
             });
-            scheduledNotifications.MapGet("/send-to-all-telegram", async (IScheduledNotificationsRepository cr, IContactsRepository cont, [FromBody] UpsertScheduledNotificationRequestForContactDto request) =>
+            scheduledNotifications.MapPost("/send-to-all-telegram", async (IScheduledNotificationsRepository cr, IContactsRepository cont, [FromBody] UpsertScheduledNotificationRequestForContactDto request) =>
             {
                 if (request is null)
                     return Results.BadRequest("Invalid request");
